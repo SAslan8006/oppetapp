a419e6c41c156269c27226faed0b4c87dc98fdad
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2022-12-31 12:49:29 +0300

    Ins: Routes insert and fixed babel.config inport reanimated

---
+++ src/navigation/Routes.js
@@ -0,0 +1,29 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+import { NavigationContainer, NavigationHelpersContext, NavigationRouteContext } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import IconMa from 'react-native-vector-icons/MaterialIcons';
+import IconFa from 'react-native-vector-icons/FontAwesome5';
+import MaC from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import HomeScreen from '../screens/Home';
+
+
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+
+
+
+const Routes = () => {
+  return (
+    <Text>Opet App Routes</Text>
+  )
+}
+
+export default Routes;
